d55b127024c1c8a91667e9471cae192d3e7e815e
Nikola Kovacs <nikola.kovacs@gmail.com>
2016-01-14 14:41:43 +0100

    fix double disconnect event on client disconnect, fixes #112

--- socket.go
+++ socket.go
@@ -137,6 +137,11 @@ func (s *socket) loop() error {
 		if err := decoder.Decode(&p); err != nil {
 			return err
 		}
+		if p.Type == _DISCONNECT {
+			// deferred function will send disconnection event.
+			// exit early to prevent double event
+			return nil
+		}
 		ret, err := s.socketHandler.onPacket(decoder, &p)
 		if err != nil {
 			return err
@@ -160,8 +165,6 @@ func (s *socket) loop() error {
 					return err
 				}
 			}
-		case _DISCONNECT:
-			return nil
 		}
 	}
 }
